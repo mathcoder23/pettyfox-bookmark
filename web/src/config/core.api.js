241b63d6313555569bd1ea93a0cb4fcce60418ba
fengwei <mathcoder23@126.com>
2021-07-22 11:59:43 +0800

    - [U] 测试部署，调整前端开发

--- web/src/config/core.api.js
+++ web/src/config/core.api.js
@@ -4,7 +4,7 @@
 import {BookmarkApi} from "../api/BookmarkApi";
 import {CoreApi} from "../core/api/core/CoreApi";
 
-CoreApi.init("http://localhost:8080")
+CoreApi.init("http://api.pettyfox.top:10004")
 export const MyApi = {
     //实现的方法写在其中
     BookmarkApi: BookmarkApi
